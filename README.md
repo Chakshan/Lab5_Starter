fb5d30c9e7046e8826c2f0617df0bd605789928e
Chakshan <chakshan.kothakota@gmail.com>
2025-05-04 10:42:51 -0700

    finish explore and check your understanding questions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+
+## Unit Testing Question
+1. I would not use a unit test to test the "message" feature of a messaging application. This is because unit tests are more suited to test individual units of code in isolation without external dependencies. Since the message feature involves external dependencies like networking, unit tests would not be an appropriate testing method.
+2. I would use a unit test to test the "max message length" feature of a messaging application. This is because this features is isolated and has deterministic desired outcomes. Hence, unit tests would be well-suited here.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,44 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  console.log("alive")
+  const synth = window.speechSynthesis;
+  let voices = synth.getVoices();
+  const textToSpeak = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const button = document.querySelector("button");
+  const image = document.querySelector("img");
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.value = `${voices[i].name}`;
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  button.addEventListener("click", (event) => {
+    let text = textToSpeak.value;
+    let utterance = new SpeechSynthesisUtterance(text);
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === voiceSelect.value) {
+        utterance.voice = voices[i];
+      }
+    }
+    synth.speak(utterance);
+    image.src = "assets/images/smiling-open.png";
+    utterance.addEventListener("end", (event) => {
+      image.src = "assets/images/smiling.png";
+    });
+  });
+
 }
